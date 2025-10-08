980b10ad02f15baf179daf8eaef5dd7a3925b04b
Jackson Case <casejack100@gmail.com>
2025-10-08 09:54:41 -0400

    Fixed Terry A. Davis memorial

--- src/main/include/lib/logging/LoggerFactory.h
+++ src/main/include/lib/logging/LoggerFactory.h
@@ -6,15 +6,15 @@
 #include <frc/smartdashboard/SmartDashboard.h>
 
 
-// A message to anyone looking at this code
-/*
+/* A message to anyone looking at this code:
+ *
  * I seriously can't tell if this is a good or a bad way to do this.
  * I wish WPI just had a Log(key, value) function, and I could forget about this.
  * But they hate me personally. So in the wise words of a profound programmer:
  * 
- *    "Why don't you go study a ******* ant colony instead? You ******* ******."
+ *    "Why don't you go study an ant colony? You ******* *******. The human species is a goddamn ant colony..."
  *          - Terry A. Davis
- * (RIP 1969-2018)
+ *            (RIP 1969-2018)
 */
 
 namespace LoggerFactory
